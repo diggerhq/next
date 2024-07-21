03aa7b3dc31d44d273891597f84f90b1f915fe46
motatoes <moe.habib9@gmail.com>
2024-07-21 12:24:20 +0100

    add missing columns for repo table

---
+++ supabase/migrations/20240721112231_digger_repos_add_missing_columns.sql
@@ -0,0 +1,4 @@
+ALTER TABLE public.repos ADD repo_name text NULL;
+ALTER TABLE public.repos ADD repo_full_name text NULL;
+ALTER TABLE public.repos ADD repo_organisation text NULL;
+ALTER TABLE public.repos ADD repo_url text NULL;
