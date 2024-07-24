afffb3fb3304d613976fa34622786a32403397fc
motatoes <moe.habib9@gmail.com>
2024-07-24 21:37:14 +0100

    renmove local edits migration for now

--- supabase/migrations/20240724152045_edits_to_encrypted_vars_table.sql
+++
@@ -1,7 +0,0 @@
-alter table "public"."encrypted_env_vars" drop column "iv";
-
-alter table "public"."encrypted_env_vars" alter column "encrypted_value" set default ''::text;
-
-alter table "public"."encrypted_env_vars" alter column "encrypted_value" set data type text using "encrypted_value"::text;
-
-
