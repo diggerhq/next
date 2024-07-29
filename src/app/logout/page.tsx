9de9523304fd8622133b07fb4bc499e22784cf50
psiddharthdesign <107192927+psiddharthdesign@users.noreply.github.com>
2024-07-29 14:12:51 +0530

    redirect on / to login or dashboard dep on session

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,15 +1,15 @@
 'use client';
 import { T } from '@/components/ui/Typography';
 import { supabaseUserClientComponentClient } from '@/supabase-clients/user/supabaseUserClientComponentClient';
-import { useDidMount } from 'rooks';
 import { useRouter } from 'next/navigation';
+import { useDidMount } from 'rooks';
 
 export default function Logout() {
   const router = useRouter();
   useDidMount(async () => {
     await supabaseUserClientComponentClient.auth.signOut();
     router.refresh();
-    router.replace('/');
+    router.replace('/login');
   });
 
   return <T.P>Signing out...</T.P>;
---
+++ src/app/route.ts
@@ -0,0 +1,33 @@
+import { createSupabaseUserRouteHandlerClient } from '@/supabase-clients/user/createSupabaseUserRouteHandlerClient';
+import { NextResponse } from 'next/server';
+
+export const dynamic = 'force-dynamic';
+
+export async function GET() {
+  const supabase = createSupabaseUserRouteHandlerClient();
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (error) {
+    console.error('Error checking user authentication:', error);
+    // In case of an error, redirect to login as a fallback
+    return NextResponse.redirect(
+      new URL('/login', process.env.NEXT_PUBLIC_SITE_URL),
+    );
+  }
+
+  if (user) {
+    // User is logged in, redirect to dashboard
+    return NextResponse.redirect(
+      new URL('/dashboard', process.env.NEXT_PUBLIC_SITE_URL),
+    );
+  } else {
+    // User is not logged in, redirect to login
+    return NextResponse.redirect(
+      new URL('/login', process.env.NEXT_PUBLIC_SITE_URL),
+    );
+  }
+}
