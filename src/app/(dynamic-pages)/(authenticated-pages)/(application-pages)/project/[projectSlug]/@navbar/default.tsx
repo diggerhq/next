714099d25bb46bed8723aeacc968aa1c0ce51b76
psiddharthdesign <107192927+psiddharthdesign@users.noreply.github.com>
2024-07-30 20:02:07 +0530

    fix / project slug fix

--- src/app/(dynamic-pages)/(authenticated-pages)/(application-pages)/project/[projectSlug]/@navbar/default.tsx
+++ src/app/(dynamic-pages)/(authenticated-pages)/(application-pages)/project/[projectSlug]/@navbar/default.tsx
@@ -1,7 +1,7 @@
 // https://github.com/vercel/next.js/issues/58272
 import { T } from '@/components/ui/Typography';
 import { Skeleton } from '@/components/ui/skeleton';
-import { getProjectById, getSlimProjectBySlug } from '@/data/user/projects';
+import { getSlimProjectBySlug } from '@/data/user/projects';
 import { projectSlugParamSchema } from '@/utils/zod-schemas/params';
 import { Layers } from 'lucide-react';
 import Link from 'next/link';
@@ -9,7 +9,7 @@ import Link from 'next/link';
 import { Suspense } from 'react';
 
 async function Title({ projectId }: { projectId: string }) {
-  const project = await getProjectById(projectId);
+  const project = await getSlimProjectBySlug(projectId);
   return (
     <div className="flex items-center gap-2">
       <Layers className="w-4 h-4" />
@@ -23,13 +23,12 @@ async function Title({ projectId }: { projectId: string }) {
 
 export default async function ProjectNavbar({ params }: { params: unknown }) {
   const { projectSlug } = projectSlugParamSchema.parse(params);
-  const project = await getSlimProjectBySlug(projectSlug);
   return (
     <div className="flex items-center">
-      <Link href={`/project/${project.id}`}>
+      <Link href={`/project/${projectSlug}`}>
         <span className="flex items-center space-x-2">
           <Suspense fallback={<Skeleton className="w-16 h-6" />}>
-            <Title projectId={project.slug} />
+            <Title projectId={projectSlug} />
           </Suspense>
         </span>
       </Link>
