ed19bd4a013fb24b5ff3b4c95bb0a393188b567e
Igor Zalutski <izalutski@gmail.com>
2024-09-03 17:33:31 +0100

    Revert data/user/projects.tsx

--- src/data/user/projects.tsx
+++ src/data/user/projects.tsx
@@ -8,7 +8,6 @@ import { createSupabaseUserServerComponentClient } from "@/supabase-clients/user
 import type { CommentWithUser, Enum, SAPayload } from "@/types";
 import { normalizeComment } from "@/utils/comments";
 import { serverGetLoggedInUser } from "@/utils/server/serverGetLoggedInUser";
-import { SupabaseClient } from "@supabase/supabase-js";
 import { revalidatePath } from "next/cache";
 import { Suspense } from "react";
 import { getRepoDetails } from "./repos";
@@ -71,7 +70,6 @@ export const createProjectAction = async ({
   slug,
   repoId,
   terraformWorkingDir,
-  branch,
   managedState,
   labels,
   teamId,
@@ -83,7 +81,6 @@ export const createProjectAction = async ({
   slug: string;
   repoId: number;
   terraformWorkingDir: string;
-  branch: string;
   managedState: boolean;
   labels: string[];
   teamId: number | null;
@@ -101,7 +98,6 @@ export const createProjectAction = async ({
       team_id: teamId,
       repo_id: repoId,
       terraform_working_dir: terraformWorkingDir,
-      branch: branch,
       is_managing_state: managedState,
       is_in_main_branch: true,
       is_generated: true,
@@ -110,6 +106,7 @@ export const createProjectAction = async ({
       labels,
       is_drift_detection_enabled,
       drift_crontab,
+
     })
     .select("*")
     .single();
@@ -896,28 +893,6 @@ export const getProjectsForUserTotalCount = async ({
 // data/user/projects.ts
 
 
-async function createCronJob(schedule: string, supabase: SupabaseClient,) {
-  const { data, error } = await supabase.rpc('cron_schedule', {
-    job_name: 'invoke-function-every-half-minute',
-    schedule,
-    function_code: `
-      select
-        net.http_post(
-            url:='https://project-ref.supabase.co/functions/v1/function-name',
-            headers:='{"Content-Type": "application/json", "Authorization": "Bearer YOUR_ANON_KEY"}'::jsonb,
-            body:=concat('{"time": "', now(), '"}')::jsonb
-        ) as request_id;
-    `
-  })
-
-  if (error) {
-    console.error('Error creating cron job:', error)
-  } else {
-    console.log('Cron job created successfully:', data)
-  }
-}
-
-
 export async function updateProjectSettingsAction({
   projectId,
   terraformWorkingDir,
@@ -950,8 +925,6 @@ export async function updateProjectSettingsAction({
       .single();
 
     if (error) throw error;
-
-    await createCronJob(drift_crontab, supabase);
 
     return { status: 'success', data };
   } catch (error) {
