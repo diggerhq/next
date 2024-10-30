fe4700caed2d12f55386c7e956ddaba1a53f9071
motatoes <moe.habib9@gmail.com>
2024-10-30 19:01:35 +0000

    commented out migrations

--- supabase/migrations/20241017151749_user_profile_naming.sql
+++ supabase/migrations/20241017151749_user_profile_naming.sql
@@ -6,8 +6,8 @@ DROP COLUMN IF EXISTS hasCreatedOrganization,
 DROP COLUMN IF EXISTS isCreatedThroughOrgInvitation;
 
 -- Recreate columns with underscore notation
-ALTER TABLE user_profiles
-ADD COLUMN has_accepted_terms BOOLEAN DEFAULT TRUE,
-ADD COLUMN has_completed_profile BOOLEAN DEFAULT FALSE,
-ADD COLUMN has_created_organization BOOLEAN DEFAULT FALSE,
-ADD COLUMN is_created_through_org_invitation BOOLEAN DEFAULT FALSE;
+-- ALTER TABLE user_profiles
+-- ADD COLUMN has_accepted_terms BOOLEAN DEFAULT TRUE,
+-- ADD COLUMN has_completed_profile BOOLEAN DEFAULT FALSE,
+-- ADD COLUMN has_created_organization BOOLEAN DEFAULT FALSE,
+-- ADD COLUMN is_created_through_org_invitation BOOLEAN DEFAULT FALSE;
